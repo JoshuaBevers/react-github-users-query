0fd34f7d0d403009d54fa963994ea97e77303e12
infernezor@gmail.com <infernezor@gmail.com>
2020-04-01 10:21:48 -0400

    Basic scaffolding setup. App forum and handleChange creation.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    textInput: "",
+    users: []
+  };
+
+  handleChange = event => {
+    this.setState({
+      textInput: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const previousInput = this.state.textInput;
+
+    this.setState({
+      textInput: "",
+      users: [...this.state.users, previousInput]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Input UserName
+            <input
+              type="text"
+              value={this.state.textInput}
+              placeholder="Text Input"
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.textInput}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/UserCardList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+function UserCardList() {}
+
+export default UserCardList;
